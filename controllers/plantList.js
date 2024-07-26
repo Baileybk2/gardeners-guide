4ef88dad10ef8466eb3672a334c5eed2c5eff304
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-26 15:53:30 -0500

    add post for plantList route

--- controllers/plantList.js
+++ controllers/plantList.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const verifyToken = require('../middleware/verify-token.js');
 const PlantList = require('../models/plantList.js');
+const Profile = require('../models/profile.js');
+const User = require('../models/user.js')
 const router = express.Router();
 
 // ========== Public Routes ===========
@@ -9,5 +11,19 @@ const router = express.Router();
 
 router.use(verifyToken);
 
+router.post('/', async (req, res) => {
+    try {
+        const plant = await PlantList.create(req.body)
 
+        const user = await User.findById(req.user._id);
+        const profile = await Profile.findOneAndUpdate(
+            {username: user._id}, 
+            {$push: {plantList: plant._id}}
+        )
+        
+        res.status(201).json({plant, profile})
+    } catch (err) {
+        console.log(err)
+    }
+})
 module.exports = router;
