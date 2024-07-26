eb0ec00ba242d755949ecca8fd2ee46c740897b8
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-26 15:21:11 -0500

    add profile create functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
 const verifyToken = require('../middleware/verify-token');
+const Profile = require('../models/profile');
 
 router.get('/:userId', verifyToken, async (req, res) => {
     try {
@@ -9,11 +10,13 @@ router.get('/:userId', verifyToken, async (req, res) => {
             return res.status(401).json({ error: "Unauthorized"})
         }
         const user = await User.findById(req.user._id);
+        const profile = await Profile.findOne({username: user._id}).populate('username')
+        
         if (!user) {
             res.status(404)
             throw new Error('Profile not found.');
         }
-        res.json({ user });
+        res.json({ user, profile });
     } catch (error) {
         if (res.statusCode === 404) {
             res.status(404).json({ error: error.message });
