85839267ba47b1f1d3f9ff30b65e7631bcde084b
BaileyBeck <baileybk2@gmail.com>
2024-08-01 19:37:49 -0400

    added origin to CORS

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+dotenv.config()
+const cors = require("cors")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
 
-const plantListRouter = require('./controllers/plantList')
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
+const plantListRouter = require("./controllers/plantList")
+const usersRouter = require("./controllers/users")
+const profilesRouter = require("./controllers/profiles")
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-app.use(cors());
-app.use(express.json());
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+app.use(cors({ origin: process.env.REACT_FRONT_END_URL }))
+app.use(express.json())
 
 // Routes go here
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
-app.use('/plantList', plantListRouter)
+app.use("/users", usersRouter)
+app.use("/profiles", profilesRouter)
+app.use("/plantList", plantListRouter)
 
 app.listen(3000, () => {
-    console.log('The express app is ready!');
-});
+  console.log("The express app is ready!")
+})
